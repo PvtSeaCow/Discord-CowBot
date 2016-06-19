0f08dcea4465fabaa5449091139cc650b9db9f93
Devon <pvtseacow@gmail.com>
2016-06-19 18:39:06 -0400

    Done I swear

--- cow.py
+++ cow.py
@@ -15,7 +15,7 @@ if not discord.opus.is_loaded():
     # you should replace this with the location the
     # opus library is located in and with the proper filename.
     # note that on windows this DLL is automatically provided for you
-    discord.opus.load_opus('libopus.1a')
+    discord.opus.load_opus(find_library('opus'))
 
 @bot.event
 async def on_ready():
