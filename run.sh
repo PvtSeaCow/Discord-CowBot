aec0f79976b8d38a8ac3edc624ca016d1c41904f
Devon <pvtseacow@gmail.com>
2016-06-23 23:03:21 -0400

    Added other update procedures

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@ while true; do
     python3.5 cow.py
     sleep 2
     git pull
+    python3.5 -m pip install --upgrade discord.py
+    python3.5 -m pip install --upgrade youtube-dl
     sleep 1
 done
