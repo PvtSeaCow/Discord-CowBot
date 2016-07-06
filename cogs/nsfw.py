a069b568aad21148a8a1855ceafbfa2549b53d8a
Devon <pvtseacow@gmail.com>
2016-07-06 13:14:10 -0400

    Made the nsfw send typing so you know its working

--- cogs/nsfw.py
+++ cogs/nsfw.py
@@ -24,6 +24,7 @@ class nsfw:
         
     @commands.command(pass_context=True, help='Send nsfw images using \'content\' as tags.\nSites you can currently use: konachan, chan, ibsearch, yandere.\nSites currently being added: Danbooru, idol.', brief='''Send nsfw images using 'content' as tags.''')
     async def lood(self, ctx, site : str, *content : str):
+        await self.bot.type()
         global base_url
         if not ('nsfw' in ctx.message.channel.name or 'fap' in ctx.message.channel.name or 'test-for-sea-cow' in ctx.message.channel.name or 'lood' in ctx.message.channel.name or 'lewd' in ctx.message.channel.name or 'illow ' in str(ctx.message.server.name) or 'hentai' in str(ctx.message.server.name) or 'lewd' in str(ctx.message.server.name) or 'lood' in str(ctx.message.server.name)):
             await self.bot.say('`This isn\'t a lood channel silly`')
